130da391056fcf5e078c568fb0203b05a7485092
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2024-12-24 07:44:15 +0000

    going rogue

--- insert_data.sh
+++ insert_data.sh
@@ -6,5 +6,21 @@ then
 else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 fi
-
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+cat gamestest.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WGOALS OGOALS
+do
+  if [[ $YEAR != 'year' ]]; then
+    # Team Table First
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$winner'")
+    if [[ -z $TEAM_ID ]]; then
+      INSERT_TEAM_RESULT=$($PSQL "INSERT INTO")
+    fi
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name ='$opponent'")
+    if [[ -z $TEAM_ID ]]; then
+      echo $TEAM_ID
+    fi
+
+  fi
+
+done
